ce4bd2d63f677db2ae30a61333805fd0d634720c
sivasanniyasi <88707151+sivasanniyasi@users.noreply.github.com>
2021-09-06 12:54:45 +0530

    Add files via upload

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using WishList.Data;
+using WishList.Models;
 namespace WishList
 {
     public class Startup
@@ -17,15 +18,40 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<options => options.UseInMemoryDatabase("WishList")>;
+
+            //services.AddTransient<IItemRepository,  MockItemRepository>();
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
+            //services.AddEntityFrameworkInMemoryDatabase();
+            //services.AddTransient<IItemData, InMemoryItemData>(); 
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
